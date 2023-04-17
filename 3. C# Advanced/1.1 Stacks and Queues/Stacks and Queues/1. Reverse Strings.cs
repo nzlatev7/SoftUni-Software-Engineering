7690509e7c4808a09b8c4d6f186ceab9409a657b
Nikolay <nikolay.zlatev2005@gmail.com>
2023-04-17 16:29:10 +0300

    upload 2 solutions
    1. Reverse a String
    2. Stack Sum

---
+++ 3. C# Advanced/1.1 Stacks and Queues/Stacks and Queues/1. Reverse Strings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _1._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var ch in input)
+            {
+                stack.Push(ch);
+            }
+
+            string output = string.Empty;
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = stack.Pop();
+                output += currentChar;
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ 3. C# Advanced/1.1 Stacks and Queues/Stacks and Queues/2. Stack Sum.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            Stack<int> nums = new Stack<int>();
+
+            foreach (var num in numbers)
+            {
+                nums.Push(num);
+            }
+
+            string command;
+            while ((command = Console.ReadLine().ToLower()) != "end")
+            {
+                string[] data = command.Split(" ");
+
+                string firstCommmand = data[0].ToLower();
+
+                if (firstCommmand == "add")
+                {
+                    int num1 = int.Parse(data[1]);
+                    int num2 = int.Parse(data[2]);
+
+                    nums.Push(num1);
+                    nums.Push(num2);
+                }
+                else if (firstCommmand == "remove")
+                {
+                    int num = int.Parse(data[1]);
+
+                    //skip
+                    if (num > nums.Count)
+                    {
+                        continue;
+                    }
+
+                    for (int i = 0; i < num; i++)
+                    {
+                        nums.Pop();
+                    }
+                }
+            }
+            Console.WriteLine("Sum: " + nums.Sum());
+        }
+    }
+}
