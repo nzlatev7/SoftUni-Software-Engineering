eb51ad6d0da0e0d2440d06ca705b7ae0dce26083
Nikolay <nikolay.zlatev2005@gmail.com>
2023-04-18 22:26:16 +0300

    upload 2 solutions for 2d arrays
    1. SumMatrixElements
    2. SumMatrixColumns

---
+++ 3. C# Advanced/2.1 Multidimensional Arrays/1. SumMatrixElements.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int rows = sizes[0];
+            int columns = sizes[1];
+
+            int[,] matrix = new int[rows, columns];
+
+            int sum = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                int[] array = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                sum += array.Sum();
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = array[j];
+                    
+                }
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(columns);
+            Console.WriteLine(sum);   
+        }
+    }
+}
---
+++ 3. C# Advanced/2.1 Multidimensional Arrays/2. SumMatrixColumns.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int rows = sizes[0];
+            int columns = sizes[1];
+
+            int[,] matrix = new int[rows, columns];
+
+            List<int> allSums = new List<int>();
+
+            for (int i = 0; i < rows; i++)
+            {
+                int[] array = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = array[j];
+
+                }
+            }
+
+            int currentSum = 0;
+            for (int i = 0; i < columns; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    currentSum += matrix[j, i];
+                }
+                allSums.Add(currentSum);
+                currentSum = 0;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, allSums));
+        }
+    }
+}
