19e7d89a648e396847c6b723bbc7d497e70cef7d
Nikolay <nikolay.zlatev2005@gmail.com>
2023-04-19 15:53:43 +0300

    upload two solutions
    4. Symbol in Matrix
    6. Jagged-Array Modification

---
+++ 3. C# Advanced/2.1 Multidimensional Arrays/4. Symbol in Matrix.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string stringOfChars = Console.ReadLine();
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = stringOfChars[j];
+                }
+            }
+
+            char symbol = char.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matrix[i,j] == symbol)
+                    {
+                        Console.WriteLine($"({i}, {j})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{symbol} does not occur in the matrix ");
+        }
+    }
+}
---
+++ 3. C# Advanced/2.1 Multidimensional Arrays/6. Jagged-ArrayModi.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //6.	Jagged-Array Modification
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] sequence = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = sequence[j];
+                }
+            }
+
+            //add
+            //subtract
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] data = command.Split(" ");
+
+                string currentOperation = data[0];
+                int row = int.Parse(data[1]);
+                int col = int.Parse(data[2]);
+                int value = int.Parse(data[3]);
+
+                //validate the indexes
+                if (row < 0 || col < 0 || row >= n || col >= n)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                    continue;
+                }
+
+                if (currentOperation == "Add")
+                {
+                    matrix[row, col] += value;
+                }
+                else if (currentOperation == "Subtract")
+                {
+                    matrix[row, col] -= value;
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
