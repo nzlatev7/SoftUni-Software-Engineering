40f9d6c945f8ee23e5f500582525ed998a2c1e61
Nikolay <nikolay.zlatev2005@gmail.com>
2023-04-20 23:28:46 +0300

    upload 2 solutions from Sets and Dictionaries Advanced - Lab
    03. Largest 3 Numbers
    04. Product Shop

---
+++ 3. C# Advanced/3.1 Sets and Dictionaries Advanced/03. Largest 3 Numbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Largest_3_Nums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            Console.WriteLine(string.Join(" ", nums.OrderByDescending(x => x).Take(3)));
+        }
+    }
+}
