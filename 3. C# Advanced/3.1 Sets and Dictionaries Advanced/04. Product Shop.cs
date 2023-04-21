01be3d79a67f565ac6d748ecd7deb5c945fa22c8
Nikolay <nikolay.zlatev2005@gmail.com>
2023-04-21 22:11:34 +0300

    change the price to double

---
+++ 3. C# Advanced/3.1 Sets and Dictionaries Advanced/04. Product Shop.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Product_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shops = new Dictionary<string, List<Product>>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Revision")
+            {
+                // input -> "{shop}, {product}, {price}"
+                string[] data = command.Split(", ");
+                string shop = data[0];
+                string productName = data[1];
+                double price = double.Parse(data[2]);
+
+                if (shops.ContainsKey(shop))
+                {
+                    Product product1 = new Product(productName, price);
+                    shops[shop].Add(product1);
+                    continue;
+                }
+
+                Product product = new Product(productName, price);
+                List<Product> products = new List<Product>();
+                products.Add(product);
+
+                shops.Add(shop, products);
+            }
+
+            foreach (var shop in shops.OrderBy(x => x.Key))
+            {
+                Console.WriteLine(shop.Key + "->");
+                foreach (var product in shop.Value)
+                {
+                    Console.WriteLine($"Product: {product.ProductName}, Price: {product.Price}");
+                }
+            }
+        }
+    }
+    class Product
+    {
+        public Product(string productName, double price)
+        {
+            ProductName = productName;
+            Price = price;
+        }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+    }
+}
