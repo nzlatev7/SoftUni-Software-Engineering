bc345c1627c5aebe62b9067c06108c645f8c8a65
Nikolay <nikolay.zlatev2005@gmail.com>
2023-04-26 23:07:00 +0300

    upload 2. Line Numbers

---
+++ 3. C# Advanced/4.1 Streams, Files and Directories/2. Line Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            StreamReader streamReader = new StreamReader(inputFilePath);
+            StreamWriter streamWriter = new StreamWriter(outputFilePath);
+
+            using (streamReader)
+            {
+                using (streamWriter)
+                {
+                    int counter = 1;
+
+                    string expresion;
+                    while ((expresion = streamReader.ReadLine()) != null)
+                    {
+                        streamWriter.WriteLine($"{counter++}. {expresion}");
+                    }
+                }
+            }
+        }
+    }
+}
