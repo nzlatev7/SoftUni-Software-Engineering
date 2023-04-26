ac625cad39479369e42485b83c810f1ad68e8b3e
Nikolay <nikolay.zlatev2005@gmail.com>
2023-04-26 23:05:55 +0300

    upload 1. Odd Lines

---
+++ 3. C# Advanced/4.1 Streams, Files and Directories/1. Odd Lines/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+
+namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            StreamReader streamReader = new StreamReader(inputFilePath);
+
+            StreamWriter writer = new StreamWriter(outputFilePath);
+
+            using (streamReader)
+            {
+                using (writer)
+                {
+                    string expresion = streamReader.ReadLine();
+
+                    int counter = 0;
+                    while (expresion != null)
+                    {
+                        if (counter % 2 != 0)
+                        {
+                            writer.WriteLine(expresion);
+                        }
+                        counter++;
+                        expresion = streamReader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
