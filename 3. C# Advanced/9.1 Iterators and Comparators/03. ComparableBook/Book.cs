9a2421826010d7bf00bc1ea8662246650d827e67
Nikolay <nikolay.zlatev2005@gmail.com>
2023-04-30 16:36:38 +0300

    upload 03. ComparableBook

---
+++ 3. C# Advanced/9.1 Iterators and Comparators/03. ComparableBook/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    class Book : IComparable<Book>
+    {
+        // here we need to use params, because we do not know how many authors will the user enter
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors.ToList();
+        }
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+
+        public List<string> Authors { get; set; }
+
+        public int CompareTo(Book other)
+        {
+            if (Year != other.Year)
+            {
+                return Year.CompareTo(other.Year);
+            }
+
+            return Title.CompareTo(other.Title);
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Year}";
+        }
+    }
+}
