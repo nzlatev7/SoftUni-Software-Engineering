add90ef1b6ff42177c49e908f538bdb308846b38
Nikolay <nikolay.zlatev2005@gmail.com>
2023-04-30 15:36:27 +0300

    upload 01. Library

---
+++ 3. C# Advanced/9.1 Iterators and Comparators/01. Library/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IteratorsAndComparators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Emil ot Lioneberq", 1963, "Astrit Lindgren");
+            Book book2 = new Book("Emil ot Lioneberq", 1963, "Astrit Lindgren", "Astrit Lindgren");
+
+            Library books = new Library(book2, book2);
+
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book.Title);
+            }
+        }
+    }
+}
