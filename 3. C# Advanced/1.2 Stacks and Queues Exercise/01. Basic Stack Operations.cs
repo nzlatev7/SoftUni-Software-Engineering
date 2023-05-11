70a1311c58b5fe8b81d382bc76f2eb939974bdb8
Nikolay <nikolay.zlatev2005@gmail.com>
2023-05-11 22:34:42 +0300

    upload 2 tasks
    01. Basic Stack Operations
    02. Basic Queue Operations

---
+++ 3. C# Advanced/1.2 Stacks and Queues Exercise/01. Basic Stack Operations.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+List<int> numsForOurTask = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+int n = numsForOurTask[0]; // nums to read
+int s = numsForOurTask[1]; // nums to pop
+int x = numsForOurTask[2]; // num that i need to found
+
+List<int> nums = Console.ReadLine().Split().Select(int.Parse).Take(n).ToList();
+
+Stack<int> stackNums = new Stack<int>(nums);
+
+for (int i = 0; i < s; i++)
+{
+    stackNums.Pop();
+}
+
+if (stackNums.Count == 0)
+{
+    Console.WriteLine(0);
+    return;
+}
+
+if (stackNums.Contains(x))
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine(stackNums.Min());
+}
---
+++ 3. C# Advanced/1.2 Stacks and Queues Exercise/02. Basic Queue Operations.cs
@@ -0,0 +1,24 @@
+﻿
+List<int> numsForOurTask = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+int n = numsForOurTask[0]; // nums to read
+int s = numsForOurTask[1]; // nums to pop
+int x = numsForOurTask[2]; // num that i need to found
+
+List<int> nums = Console.ReadLine().Split().Select(int.Parse).Take(n).ToList();
+
+Queue<int> lastNums = new Queue<int>(nums);
+
+for (int i = 0; i < s; i++)
+{
+    lastNums.Dequeue();
+}
+
+if (lastNums.Count == 0)
+{
+    Console.WriteLine(0);
+    return;
+}
+
+Console.WriteLine(lastNums.Contains(x) ? "true" : lastNums.Min());
+
