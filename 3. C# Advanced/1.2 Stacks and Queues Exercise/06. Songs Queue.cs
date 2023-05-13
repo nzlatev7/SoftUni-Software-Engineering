3964e642639728377996385b2d2f7296f97acd62
Nikolay <nikolay.zlatev2005@gmail.com>
2023-05-13 13:48:36 +0300

    upload from Stacks and Queues
    06. Songs Queue
    08. Balanced Parenthesis

---
+++ 3. C# Advanced/1.2 Stacks and Queues Exercise/06. Songs Queue.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Text;
+
+List<string> strings = Console.ReadLine().Split(", ").ToList();
+Queue<string> songs = new Queue<string>(strings);
+
+while (songs.Any()) // while songs.Count > 0
+{
+    string[] data = Console.ReadLine().Split(" ");
+
+    string command = data[0];
+
+    if (command == "Play")
+    {
+        songs.Dequeue();
+    }
+    else if (command == "Add")
+    {
+        string song = string.Empty;
+        if (data.Length > 2)
+        {
+            List<string> words = data.Skip(1).ToList();
+            song = string.Join(" ", words);
+            //song = SubString(data);
+        }
+        else
+        {
+            song = data[1];
+        }
+        
+        if (songs.Contains(song))
+        {
+            Console.WriteLine($"{song} is already contained!");
+            continue;
+        }
+
+        songs.Enqueue(song);
+    }
+    else if (command == "Show")
+    {
+        Console.WriteLine(string.Join(", ", songs));
+    }
+}
+
+Console.WriteLine("No more songs!");
---
+++ 3. C# Advanced/1.2 Stacks and Queues Exercise/08. Balanced Parenthesis.cs
@@ -0,0 +1,53 @@
+﻿
+string chars = Console.ReadLine(); //1 2 3 4 5 6
+
+Stack<char> stack = new Stack<char>();
+
+bool isValidExpression = true;
+
+foreach (var ch in chars)
+{
+
+    if (ch == '{' || ch == '(' || ch == '[')
+    {
+        stack.Push(ch);
+    }
+    else if (ch == '}')
+    {
+        if (stack.Count == 0 || stack.Pop() != '{')
+        {
+            isValidExpression = false;
+            break;
+        }
+    }
+    else if (ch == ')')
+    {
+        if (stack.Count == 0 ||stack.Pop() != '(')
+        {
+            isValidExpression = false;
+            break;
+        }
+    }
+    else if (ch == ']')
+    {
+        if (stack.Count == 0 || stack.Pop() != '[')
+        {
+            isValidExpression = false;
+            break;
+        }
+    }
+}
+//if (stack.Count != 0)
+//{
+//    isValidExpression = false;
+//}
+
+
+if (isValidExpression)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
