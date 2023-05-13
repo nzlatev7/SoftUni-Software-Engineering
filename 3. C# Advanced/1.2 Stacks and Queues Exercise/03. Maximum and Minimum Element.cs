10550379edd2ced8d5f184df9cb8e0c117054e50
Nikolay <nikolay.zlatev2005@gmail.com>
2023-05-13 12:26:10 +0300

    upload two tasks from Stacks and Queues
    03. Maximum and Minimum Element
    05. Fashion Boutique

---
+++ 3. C# Advanced/1.2 Stacks and Queues Exercise/03. Maximum and Minimum Element.cs
@@ -0,0 +1,36 @@
+﻿
+Stack<int> nums = new Stack<int>();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int[] commandAndNum = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    int commandNum = commandAndNum[0];
+
+    if (commandNum == 1)
+    {
+        int num = commandAndNum[1];
+        nums.Push(num);
+    }
+    else if (commandNum == 2)
+    {
+        nums.Pop();
+    }
+    else if (commandNum == 3)
+    {
+        if (nums.Any())
+        {
+            Console.WriteLine(nums.Max());
+        }
+    }
+    else if (commandNum == 4)
+    {
+        if (nums.Any())
+        {
+            Console.WriteLine(nums.Min());
+        }
+    }
+}
+Console.WriteLine(String.Join(", ", nums));
---
+++ 3. C# Advanced/1.2 Stacks and Queues Exercise/05. Fashion Boutique.cs
@@ -0,0 +1,27 @@
+﻿
+
+Stack<int> clothes = new Stack<int>(
+    Console.ReadLine()
+        .Split()
+        .Select(int.Parse));
+
+int capacityOfRack = int.Parse(Console.ReadLine());
+
+int sum = 0;
+int length = clothes.Count;
+
+int rackCounter = 1;
+
+for (int i = 0; i < length; i++)
+{
+    int current = clothes.Pop();
+
+    if(sum + current > capacityOfRack)
+    {
+        sum = 0;
+        rackCounter++;
+    }
+
+    sum += current;
+}
+Console.WriteLine(rackCounter);
