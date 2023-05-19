a970985de46acdda60cebf296d714bfa8423b3c8
Nikolay <nikolay.zlatev2005@gmail.com>
2023-05-19 22:33:55 +0300

    2 exercises
    9. Miner
    10. Radioactive Mutant Vampire Bunnies

---
+++ 3. C# Advanced/2.2 Multidimensional Arrays Exercise/10. Radioactive Mutant Vampire Bunnies.cs
@@ -0,0 +1,134 @@
+﻿
+
+string[] dimensions = Console.ReadLine().Split();
+
+int rows = int.Parse(dimensions[0]);
+int cols = int.Parse(dimensions[1]);
+
+char[,] lair = new char[rows, cols];
+
+int playerRow = 0;
+int playerCol = 0;
+
+for (int row = 0; row < rows; row++)
+{
+    string elements = Console.ReadLine();
+
+    for (int col = 0; col < cols; col++)
+    {
+        lair[row, col] = elements[col];
+
+        if (elements[col] == 'P')
+        {
+            playerRow = row;
+            playerCol = col;
+
+            //set the player as a dot, to be easy later
+            lair[playerRow, playerCol] = '.';
+        }
+    }
+}
+
+//get the directions
+string directions = Console.ReadLine();
+
+int lastPlayerRow = playerRow;
+int lastPlayerCol = playerCol;
+
+foreach (var direction in directions)
+{
+    lastPlayerRow = playerRow;
+    lastPlayerCol = playerCol;
+
+    switch (direction)
+    {
+        case 'L':
+            playerCol--;
+            break;
+        case 'R':
+            playerCol++;
+            break;
+        case 'U':
+            playerRow--;
+            break;
+        case 'D':
+            playerRow++;
+            break;
+    }
+
+    lair = SpreadBunnies(lair, rows, cols);
+
+    if (playerRow < 0
+        || playerRow >= rows
+        || playerCol < 0
+        || playerCol >= cols)
+    {
+        break;
+    }
+
+    if (lair[playerRow, playerCol] == 'B')
+    {
+        Print(lair, rows, cols);
+        Console.WriteLine($"dead: {playerRow} {playerCol}");
+        return;
+    }
+}
+Print(lair, rows, cols);
+Console.WriteLine($"won: {lastPlayerRow} {lastPlayerCol}");
+
+static char[,] SpreadBunnies(char[,] lair, int rows, int cols)
+{
+    char[,] newLair = new char[rows, cols];
+
+    for (int row = 0; row < rows; row++)
+    {
+        for (int col = 0; col < cols; col++)
+        {
+            newLair[row, col] = lair[row, col];
+        }
+    }
+
+    for (int row = 0; row < rows; row++)
+    {
+        for (int col = 0; col < cols; col++)
+        {
+            if (lair[row, col] == 'B')
+            {
+                if (row > 0) //up
+                {
+                    newLair[row - 1, col] = 'B';
+                }
+
+                if (row < rows - 1) //down
+                {
+                    newLair[row + 1, col] = 'B';
+                }
+
+                if (col > 0) //left
+                {
+                    newLair[row, col - 1] = 'B';
+                }
+
+                if (col < cols - 1) //right
+                {
+                    newLair[row, col + 1] = 'B';
+                }
+            }
+        }
+    }
+
+    return newLair;
+}
+
+static void Print(char[,] lair, int rows, int cols)
+{
+    for (int row = 0; row < rows; row++)
+    {
+        for (int col = 0; col < cols; col++)
+        {
+            Console.Write(lair[row, col]);
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ 3. C# Advanced/2.2 Multidimensional Arrays Exercise/9. Miner.cs
@@ -0,0 +1,89 @@
+﻿
+int size = int.Parse(Console.ReadLine());
+
+char[,] field = new char[size, size];
+
+string[] directions = Console.ReadLine().Split();
+
+int coals = 0;
+
+int minerRowIndex = 0;
+int minerColIndex = 0;
+
+for (int row = 0; row < size; row++)
+{
+    char[] elements = Console.ReadLine()
+        .Split()
+        .Select(char.Parse)
+        .ToArray();
+
+    for (int col = 0; col < size; col++)
+    {
+        field[row, col] = elements[col];
+
+        if (elements[col] == 'c')
+        {
+            coals++;
+        }
+        if (elements[col] == 's')
+        {
+            minerRowIndex = row;
+            minerColIndex = col;
+        }
+    }
+}
+
+int collectedCoal = 0;
+
+foreach (var direction in directions)
+{
+    switch (direction)
+    {
+        case "up":
+            if (minerRowIndex > 0) //up
+            {
+                minerRowIndex--;
+            }
+            break;
+        case "down":
+            if (minerRowIndex < size - 1) //down
+            {
+                minerRowIndex++;
+            }
+            break;
+        case "left":
+            if (minerColIndex > 0) //left
+            {
+                minerColIndex--;
+            }
+            break;
+        case "right":
+            if (minerColIndex < size - 1) //right
+            {
+                minerColIndex++;
+            }
+            break;
+    }
+    
+
+    if (field[minerRowIndex, minerColIndex] == 'c')
+    {
+        collectedCoal++;
+        field[minerRowIndex, minerColIndex] = '*';
+    }
+
+    //if collecting all coals
+    if (coals == collectedCoal)
+    {
+        Console.WriteLine($"You collected all coals! ({ minerRowIndex }, { minerColIndex })");
+        return;
+    }
+
+    // if current = 'e' - end game
+    if (field[minerRowIndex, minerColIndex] == 'e')
+    {
+        Console.WriteLine($"Game over!({ minerRowIndex}, { minerColIndex })");
+        return;
+    }
+}
+Console.WriteLine($"{coals - collectedCoal} coals left. ({minerRowIndex}, {minerColIndex})");
