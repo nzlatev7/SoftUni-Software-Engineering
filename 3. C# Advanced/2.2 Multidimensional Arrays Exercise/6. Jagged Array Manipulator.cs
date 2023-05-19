aa5f90ab4877aca090b655b722fd44bd99efd93f
Nikolay <nikolay.zlatev2005@gmail.com>
2023-05-19 22:33:10 +0300

    upload 2 tasks form 2d Arrays
    6. Jagged Array Manipulator
    8. Bombs

---
+++ 3. C# Advanced/2.2 Multidimensional Arrays Exercise/6. Jagged Array Manipulator.cs
@@ -0,0 +1,87 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+int[][] jaggedArray = new int[n][];
+
+for (int row = 0; row < n; row++)
+{
+    int[] nums = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    jaggedArray[row] = nums;
+
+    if (row > 0)
+    {
+        if (jaggedArray[row - 1].Length == jaggedArray[row].Length)
+        {
+            Multiply(jaggedArray[row - 1]);
+            Multiply(jaggedArray[row]);
+        }
+        else
+        {
+            Divede(jaggedArray[row - 1]);
+            Divede(jaggedArray[row]);
+        }
+    }
+}
+
+string command;
+while ((command = Console.ReadLine()) != "End")
+{
+    string[] data = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string firstCommand = data[0];
+
+    int currentRow = int.Parse(data[1]);
+    int currentCol = int.Parse(data[2]);
+    int value = int.Parse(data[3]);
+
+
+    if (currentRow < 0 || currentRow >= jaggedArray.Length
+        || currentCol < 0 || currentCol >= jaggedArray[currentRow].Length)
+    {
+        continue;
+    }
+
+    if (firstCommand == "Add")
+    {
+        jaggedArray[currentRow][currentCol] += value;
+    }
+    else if (firstCommand == "Subtract")
+    {
+        jaggedArray[currentRow][currentCol] -= value;
+    }
+}
+
+Print(jaggedArray);
+
+
+//Methods
+static void Print(int[][] jaggedArray)
+{
+    for (int row = 0; row < jaggedArray.Length; row++)
+    {
+        for (int col = 0; col < jaggedArray[row].Length; col++)
+        {
+            Console.Write(jaggedArray[row][col] + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
+static void Multiply(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] += array[i];
+    }
+}
+static void Divede(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] /= 2;
+    }
+}
---
+++ 3. C# Advanced/2.2 Multidimensional Arrays Exercise/8. Bombs.cs
@@ -0,0 +1,131 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[n, n];
+
+for (int row = 0; row < n; row++)
+{
+    int[] elements = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    for (int col = 0; col < elements.Length; col++)
+    {
+        matrix[row, col] = elements[col];
+    }
+}
+
+string[] allIndexes = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+foreach (var currentIndexes in allIndexes)
+{
+    int[] indexes = currentIndexes.Split(",", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+
+    int currentRow = indexes[0];
+    int currentCol = indexes[1];
+
+    int value = matrix[currentRow, currentCol];
+
+    if (value > 0)
+    {
+        matrix[currentRow, currentCol] = 0;
+
+        if (currentRow > 0) //up
+        {
+            if (IsAlive(matrix[currentRow - 1, currentCol]))
+            {
+                matrix[currentRow - 1, currentCol] -= value;
+            }
+        }
+        if (currentRow < n - 1) //down
+        {
+            if (IsAlive(matrix[currentRow + 1, currentCol]))
+            {
+                matrix[currentRow + 1, currentCol] -= value;
+            }
+        }
+        if (currentCol > 0) //left
+        {
+            if (IsAlive(matrix[currentRow, currentCol - 1]))
+            {
+                matrix[currentRow, currentCol - 1] -= value;
+            }
+        }
+        if (currentCol < n - 1) //right
+        {
+            if (IsAlive(matrix[currentRow, currentCol + 1]))
+            {
+                matrix[currentRow, currentCol + 1] -= value;
+            }
+        }
+
+        //diagonals
+        if (currentRow > 0 && currentCol > 0) //*
+        {
+            if (IsAlive(matrix[currentRow - 1, currentCol - 1]))
+            {
+                matrix[currentRow - 1, currentCol - 1] -= value;
+            }
+
+        }
+        if (currentRow > 0 && currentCol < n - 1) // *
+        {
+            if (IsAlive(matrix[currentRow - 1, currentCol + 1]))
+            {
+                matrix[currentRow - 1, currentCol + 1] -= value;
+            }
+        }
+
+        if (currentCol > 0 && currentRow < n - 1) //.
+        {
+            if (IsAlive(matrix[currentRow + 1, currentCol - 1]))
+            {
+                matrix[currentRow + 1, currentCol - 1] -= value;
+            }
+        }
+        if (currentCol < n - 1 && currentRow < n - 1) // .
+        {
+            if (IsAlive(matrix[currentRow + 1, currentCol + 1]))
+            {
+                matrix[currentRow + 1, currentCol + 1] -= value;
+            }
+        }
+    }
+}
+
+int aliveCells = 0;
+int sumOfAlive = 0;
+for (int row = 0; row < n; row++)
+{
+    for (int col = 0; col < n; col++)
+    {
+        if (matrix[row, col] > 0)
+        {
+            aliveCells++;
+            sumOfAlive+= matrix[row, col];
+        }
+    }
+}
+
+Console.WriteLine($"Alive cells: {aliveCells}");
+Console.WriteLine($"Sum: {sumOfAlive}");
+
+for (int row = 0; row < n; row++)
+{
+    for (int col = 0; col < n; col++)
+    {
+            Console.Write(matrix[row, col] + " ");
+    }
+
+    Console.WriteLine();
+}
+
+
+static bool IsAlive(int num)
+{
+    return num > 0;
+}
