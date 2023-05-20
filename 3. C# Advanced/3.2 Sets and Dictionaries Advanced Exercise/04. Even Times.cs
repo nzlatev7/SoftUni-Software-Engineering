a391437baf58b99c3e588054b557c89fb4ba6491
Nikolay <nikolay.zlatev2005@gmail.com>
2023-05-20 14:17:51 +0300

    upload 2 solutions from Dictionaries
    04. Even Times
    05. Count Symbols

---
+++ 3. C# Advanced/3.2 Sets and Dictionaries Advanced Exercise/04. Even Times.cs
@@ -0,0 +1,22 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+Dictionary<int, int> nums = new Dictionary<int, int>();
+
+int evenTimeNum = 0;
+
+for (int i = 0; i < n; i++)
+{
+    int currentNum = int.Parse(Console.ReadLine());
+
+    if (nums.ContainsKey(currentNum))
+    {
+        nums[currentNum]++;
+    }
+    else
+    {
+        nums.Add(currentNum, 1);
+    }
+}
+
+Console.WriteLine(nums.Where(x => x.Value % 2 == 0).First().Key);
---
+++ 3. C# Advanced/3.2 Sets and Dictionaries Advanced Exercise/05. Count Symbols.cs
@@ -0,0 +1,21 @@
+﻿
+string text = Console.ReadLine();
+
+Dictionary<char, int> characterCount = new Dictionary<char, int>();
+
+foreach (var ch in text)
+{
+    if (characterCount.ContainsKey(ch))
+    { 
+        characterCount[ch]++;
+    }
+    else
+    {
+        characterCount.Add(ch, 1);
+    }
+}
+
+foreach (var current in characterCount.OrderBy(x => x.Key))
+{
+    Console.WriteLine($"{current.Key}: {current.Value} time/s");
+}
