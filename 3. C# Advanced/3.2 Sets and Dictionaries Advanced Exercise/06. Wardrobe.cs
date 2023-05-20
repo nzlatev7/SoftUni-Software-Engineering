6431842f1358a9338cdfa5ea21ad58e9b84d14c5
Nikolay <nikolay.zlatev2005@gmail.com>
2023-05-20 18:39:33 +0300

    upload 2 tasks from Dictionaries
    06. Wardrobe
    07. The V-Logger

---
+++ 3. C# Advanced/3.2 Sets and Dictionaries Advanced Exercise/06. Wardrobe.cs
@@ -0,0 +1,79 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] data = Console.ReadLine().Split(" -> ");
+
+    string color = data[0];
+
+    List<string> clothesForAdding = new List<string>(data[1].Split(",", StringSplitOptions.RemoveEmptyEntries));
+
+    if (wardrobe.ContainsKey(color))
+    {
+        foreach (var garment in clothesForAdding)
+        {
+            if (wardrobe[color].ContainsKey(garment))
+            {
+                wardrobe[color][garment]++;
+            }
+            else
+            {
+                wardrobe[color].Add(garment, 1);
+            }
+        }
+    }
+    else
+    {
+        Dictionary<string, int> clothes = new Dictionary<string, int>();
+        foreach (var garment in clothesForAdding)
+        {
+            if (!clothes.ContainsKey(garment))
+            {
+                clothes.Add(garment, 1);
+            }
+            
+        }
+
+        wardrobe.Add(color, clothes);
+    }
+}
+
+string[] item = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+string searchingColor = item[0];
+string seachingGarment = item[1];
+
+bool isFoundColor = false;
+bool isFoundGarment = false;
+
+foreach (var garment in wardrobe)
+{
+    if (searchingColor == garment.Key)
+    {
+        isFoundColor = true;
+    }
+
+    Console.WriteLine($"{garment.Key} clothes:");
+
+    foreach (var current in garment.Value)
+    {
+        if (current.Key == seachingGarment)
+        {
+            isFoundGarment = true;
+        }
+
+        if (isFoundColor && isFoundGarment)
+        {
+            Console.WriteLine($"* {current.Key} - {current.Value} (found!)");
+
+            isFoundColor = false;
+            isFoundGarment = false;
+
+            continue;
+        }
+
+        Console.WriteLine($"* {current.Key} - {current.Value}");
+    }
+}
---
+++ 3. C# Advanced/3.2 Sets and Dictionaries Advanced Exercise/07. The V-Logger.cs
@@ -0,0 +1,79 @@
+﻿
+Dictionary<string, Vlogger> pull = new Dictionary<string, Vlogger>();
+
+string command;
+while ((command = Console.ReadLine()) != "Statistics")
+{
+    string[] data = command.Split();
+
+    if (data[1] == "joined")
+    {
+        string username = data[0];
+
+        if (!pull.ContainsKey(username))
+        {
+            pull.Add(username, new Vlogger(username));
+        }   
+    }
+    else if (data[1] == "followed")
+    { 
+        string follower = data[0];
+        string following = data[2];
+
+        //can not follow himself
+        if (follower == following)
+        {
+            continue;
+        }
+        if (pull.ContainsKey(follower))
+        {
+            if (pull[follower].Following.Contains(following))
+            {
+                continue;
+            }
+        } 
+
+        if (pull.ContainsKey(follower) && pull.ContainsKey(following))
+        {
+            pull[follower].Following.Add(following);
+            pull[following].Followers.Add(follower);
+        }
+    }
+}
+
+Console.WriteLine($"The V-Logger has a total of {pull.Count} vloggers in its logs.");
+
+int i = 1;
+bool isFirst = true;
+
+foreach (var vlogger in pull.Values
+    .OrderByDescending(x => x.Followers.Count)
+    .ThenBy(x => x.Following.Count))
+{
+    Console.WriteLine($"{i++}. { vlogger.UserName} : { vlogger.Followers.Count} followers, { vlogger.Following.Count} following");
+
+    if (isFirst)
+    {
+        foreach (var follower in vlogger.Followers
+            .OrderBy(x => x))
+        {
+            Console.WriteLine($"*  {follower}");
+        }
+
+        isFirst = false;
+    }
+}
+
+public class Vlogger
+{
+    public Vlogger(string username)
+    {
+        UserName = username;
+        Followers = new List<string>();
+        Following = new List<string>();
+    }
+    public string UserName { get; set; }
+
+    public List<string> Followers { get; set; }
+    public List<string> Following { get; set; }
+}
