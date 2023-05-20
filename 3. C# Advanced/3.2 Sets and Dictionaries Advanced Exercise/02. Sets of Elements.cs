aa6ffc6569183f11b080d0a387c014d8a6951bf8
Nikolay <nikolay.zlatev2005@gmail.com>
2023-05-20 14:16:37 +0300

    upload 3 tasks from sets and Dictionaries
    01. Unique Usernames
    02. Sets of Elements
    03. Periodic Table

---
+++ 3. C# Advanced/3.2 Sets and Dictionaries Advanced Exercise/02. Sets of Elements.cs
@@ -0,0 +1,33 @@
+﻿
+string[] dimensions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int n = int.Parse(dimensions[0]);
+int m = int.Parse(dimensions[1]);
+
+HashSet<int> firstSet = new HashSet<int>();
+HashSet<int> secondSet = new HashSet<int>();
+
+for (int i = 0; i < n; i++)
+{
+    int currentNum = int.Parse(Console.ReadLine());
+    firstSet.Add(currentNum);
+}
+for (int i = 0; i < m; i++)
+{
+    int currentNum = int.Parse(Console.ReadLine());
+    secondSet.Add(currentNum);
+}
+
+foreach (var num1 in firstSet)
+{
+    foreach (var num2 in secondSet)
+    {
+        if (num1 == num2)
+        {
+            Console.Write(num1 + " ");
+        }
+    }
+}
+
+//or using IntersectWith
+//firstSet.IntersectWith(secondSet);
+//Console.WriteLine(String.Join(" ", firstSet));
---
+++ 3. C# Advanced/3.2 Sets and Dictionaries Advanced Exercise/03. Periodic Table.cs
@@ -0,0 +1,21 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+HashSet<string> set = new HashSet<string>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] chemicals = Console.ReadLine().Split();
+
+    for (int j = 0; j < chemicals.Length; j++)
+    {
+        set.Add(chemicals[j]);
+
+        // UnionWith as alternative solution
+    }
+}
+
+foreach (var chemical in set.OrderBy(x => x))
+{
+    Console.Write(chemical + " ");
+}
