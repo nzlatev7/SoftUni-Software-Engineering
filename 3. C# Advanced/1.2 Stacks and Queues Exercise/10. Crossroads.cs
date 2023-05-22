a9826f7ccbcfc19dd7b30e90ba86ccb1f8f0ef3a
Nikolay <nikolay.zlatev2005@gmail.com>
2023-05-22 22:54:34 +0300

    upload from Stacks and Queues - Exercise

---
+++ 3. C# Advanced/1.2 Stacks and Queues Exercise/10. Crossroads.cs
@@ -0,0 +1,49 @@
+﻿
+int durGreenLigth = int.Parse(Console.ReadLine()); //durationOfGreenLigth
+
+int durFreeWindow = int.Parse(Console.ReadLine()); //durationOfFreeWindow
+
+
+Queue<string> cars = new Queue<string>();
+
+int carCount = 0;
+
+string command;
+while ((command = Console.ReadLine()) != "END")
+{
+    if (command != "green")
+    {
+        cars.Enqueue(command);
+        continue;
+    }
+
+    int currentGreen = durGreenLigth;
+
+    while (currentGreen > 0 && cars.Count > 0)
+    {
+        string currentCar = cars.Dequeue();
+
+        if (currentGreen - currentCar.Length >= 0)
+        {
+            currentGreen -= currentCar.Length;
+            carCount++;
+            continue;
+        }
+
+        if (currentGreen + durFreeWindow - currentCar.Length >=  0)
+        {
+            carCount++;
+            continue;
+        }
+
+
+        int hittedChar = currentGreen + durFreeWindow;
+
+        Console.WriteLine("A crash happened!");
+        Console.WriteLine($"{currentCar} was hit at {currentCar[hittedChar]}.");
+
+        return;
+    }
+}
+Console.WriteLine("Everyone is safe.");
+Console.WriteLine($"{carCount} total cars passed the crossroads.");
---
+++ 3. C# Advanced/1.2 Stacks and Queues Exercise/11. Key Revolver.cs
@@ -0,0 +1,53 @@
+﻿
+int priceOfBullet = int.Parse(Console.ReadLine());
+int gunBarrel = int.Parse(Console.ReadLine());
+
+List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+Stack<int> bullets = new Stack<int>(nums);
+
+Queue<int> locks = new(
+    Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray());
+
+int intelligence = int.Parse(Console.ReadLine());
+
+int gunBarrelSupport = 0;
+int shootsCount = 0;
+int initialBulletsCount = bullets.Count;
+
+for (int i = 0; i < initialBulletsCount; i++)
+{
+    if (bullets.Pop() <= locks.Peek())
+    {
+        Console.WriteLine("Bang!");
+        locks.Dequeue();
+    }
+    else
+    {
+        Console.WriteLine("Ping!");
+    }
+
+    shootsCount++;
+    gunBarrelSupport++;
+
+    if (gunBarrelSupport == gunBarrel && bullets.Any())
+    {
+        Console.WriteLine("Reloading!");
+        gunBarrelSupport = 0;
+    }
+
+    if (!bullets.Any() && locks.Any())
+    {
+        Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+        return;
+    }
+
+    if (!locks.Any())
+    {
+        break;
+    }
+}
+Console.WriteLine($"{bullets.Count} bullets left. Earned ${intelligence - shootsCount * priceOfBullet}");
