ccd866ffef34a3e693b00e2638c8f66731eae847
Nikolay <nikolay.zlatev2005@gmail.com>
2023-05-22 15:00:04 +0300

    upload two tasks from Dictionaries

---
+++ 3. C# Advanced/3.2 Sets and Dictionaries Advanced Exercise/09. SoftUni Exam Results.cs
@@ -0,0 +1,59 @@
+﻿
+Dictionary<string, int> participantsPoints = new();
+Dictionary<string, int> submissions = new();
+
+string command;
+while ((command = Console.ReadLine()) != "exam finished")
+{
+    string[] data = command.Split("-");
+
+    if (data.Length == 2)
+    {
+        string username = data[0];
+        participantsPoints.Remove(username);
+    }
+    else
+    {
+        string username = data[0];
+        string language = data[1];
+        int points = int.Parse(data[2]);
+
+        if (!participantsPoints.ContainsKey(username))
+        {
+            if (submissions.ContainsKey(language))
+            {
+                submissions[language]++;
+            }
+            else
+            {
+                submissions.Add(language, 1);
+            }
+            
+            participantsPoints.Add(username, points);
+        }
+        else
+        {
+            submissions[language]++;
+
+            if (participantsPoints[username] < points)
+            {
+                participantsPoints[username] = points;
+            }
+        }
+    }
+}
+
+Console.WriteLine("Results:");
+foreach (var user in participantsPoints
+    .OrderByDescending(x => x.Value)
+    .ThenBy(x => x.Key))
+{
+    Console.WriteLine($"{user.Key} | {user.Value}");
+}
+
+Console.WriteLine("Submissions:");
+foreach (var current in submissions
+    .OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+{
+    Console.WriteLine($"{current.Key} - {current.Value}");
+}
---
+++ 3. C# Advanced/3.2 Sets and Dictionaries Advanced Exercise/10. ForceBook.cs
@@ -0,0 +1,70 @@
+﻿
+Dictionary<string, List<string>> sidesUsers = new();
+
+string command;
+while ((command = Console.ReadLine()) != "Lumpawaroo")
+{
+
+    if (command.Contains("|"))
+    {
+        string[] data = command.Split(" | ", StringSplitOptions.RemoveEmptyEntries);
+
+        string forceSide = data[0];
+        string forceUser = data[1];
+
+        if (!sidesUsers.Values.Any(x => x.Contains(forceUser)))
+        {
+            if (!sidesUsers.ContainsKey(forceSide))
+            {
+                sidesUsers.Add(forceSide, new List<string>());;
+            }
+
+            sidesUsers[forceSide].Add(forceUser);  
+        }
+    }
+    else
+    {
+        string[] data = command.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+        string forceUser = data[0];
+        string forceSide = data[1];
+
+        foreach (var sideUsers in sidesUsers)
+        {
+            if (sideUsers.Value.Contains(forceUser))
+            {
+                string sideForUser = sideUsers.Key;
+
+                sideUsers.Value.Remove(forceUser);
+
+                if (sidesUsers[sideForUser].Count == 0)
+                {
+                    sidesUsers.Remove(sideForUser);
+                }
+                break;
+            }
+        }
+
+        if (!sidesUsers.ContainsKey(forceSide))
+        {
+            sidesUsers.Add(forceSide, new List<string>());
+        }
+
+        sidesUsers[forceSide].Add(forceUser);
+
+        Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+    }
+}
+
+foreach (var side in sidesUsers
+    .OrderByDescending(x => x.Value.Count)
+    .ThenBy(x => x.Key))
+{
+    Console.WriteLine($"Side: {side.Key}, Members: {side.Value.Count}");
+
+    foreach (var user in side.Value
+        .OrderBy(x => x))
+    {
+        Console.WriteLine($"! {user}");
+    }
+}
