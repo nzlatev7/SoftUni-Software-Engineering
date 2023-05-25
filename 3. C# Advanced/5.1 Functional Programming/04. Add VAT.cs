16ed39abe56e12a353964cb1acb3b8b83afc4e4d
Nikolay <nikolay.zlatev2005@gmail.com>
2023-05-25 09:54:02 +0300

    upload 2 tasks form Functional Programming
    Func, Action

---
+++ 3. C# Advanced/5.1 Functional Programming/04. Add VAT.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> prices = Console.ReadLine().Split(", ").Select(double.Parse).ToList();
+
+            Func<double, double> addVat = AddingVAT;
+
+            foreach (var price in prices)
+            {
+                
+                double newPrice = addVat(price);
+                Console.WriteLine($"{newPrice:f2}");
+                //Console.WriteLine(Math.Round(newPrice, 2));
+            }
+        }
+        static double AddingVAT(double price)
+        {
+            return price += price * 0.20;
+        }
+    }
+}
---
+++ 3. C# Advanced/5.1 Functional Programming/05. Filter By Age.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Person> people = ReadPeople(n);
+
+            string condition = Console.ReadLine();
+
+            int ageThreshold = int.Parse(Console.ReadLine());
+
+            string format = Console.ReadLine();
+
+            Func<Person, bool> filter = (person) =>
+            {
+                if (condition == "younger")
+                {
+                    if (person.Age < ageThreshold)
+                    {
+                        return true;
+                    }
+                }
+                else if (condition == "older")
+                {
+                    if (person.Age >= ageThreshold)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            };
+
+            Action<Person> printer = (person) =>
+            {
+                if (format.Length > 6)
+                {
+                    Console.WriteLine($"{person.Name} - {person.Age}");
+                }
+                else if (format.Length == 3)
+                {
+                    Console.WriteLine($"{person.Age}");
+                }
+                else
+                {
+                    Console.WriteLine($"{person.Name}");
+                }
+            };
+
+            foreach (var person in people)
+            {
+                if (filter(person))
+                {
+                    printer(person);
+                }
+            }
+        }
+
+        static List<Person> ReadPeople(int n)
+        {
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] currentPerson = Console.ReadLine().Split(", ");
+
+                string name = currentPerson[0];
+                int age = int.Parse(currentPerson[1]);
+
+                Person person = new Person(name, age);
+                people.Add(person);
+            }
+
+            return people;
+        }
+    }
+    class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
