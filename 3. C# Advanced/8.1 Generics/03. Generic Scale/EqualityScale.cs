5f97fd583be9cbe71107bfb08a88ae37b53a4c45
Nikolay <nikolay.zlatev2005@gmail.com>
2023-06-03 15:27:46 +0300

    upload all tasks from Generic

---
+++ 3. C# Advanced/8.1 Generics/03. Generic Scale/EqualityScale.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T> where T : IComparable
+    {
+        public EqualityScale(T left, T rigth)
+        {
+            Left = left;
+            Right = rigth;
+        }
+
+        public T Left { get; set; }
+        public T Right { get; set; }
+
+        public bool AreEqual()
+        {
+            if (Left.CompareTo(Right) == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ 3. C# Advanced/8.1 Generics/03. Generic Scale/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<string> equalityScale = new EqualityScale<string>("a", "a");
+
+            Console.WriteLine(equalityScale.AreEqual());
+        }
+    }
+}
