f3e996d577cfd2893dd98fc0e691ce30062bbe74
Nikolay <nikolay.zlatev2005@gmail.com>
2023-06-04 10:40:50 +0300

    upload workshop - Custom Doubly Linked List

---
+++ 3. C# Advanced/7.1 Implementing Linked List/CustomLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDoublyLinkedList
+{
+    public class DoublyLinkedList
+    {
+        private ListNode head;
+        private ListNode tail;
+
+        public ListNode First
+        {
+            get { return head; }
+        }
+        public ListNode Last
+        {
+            get { return tail; }
+        }
+
+        public int Count { get; private set; }
+        public void AddFirst(int element)
+        {
+            if (Count == 0)
+            {
+                head = tail = new ListNode(element);
+            }
+            else
+            {
+                var newHead = new ListNode(element);
+                newHead.Next = head;
+                head.Previous = newHead;
+                head = newHead;
+            }
+
+            Count++;
+        }
+        public void AddLast(int element)
+        {
+            if (Count == 0)
+            {
+                head = tail = new ListNode(element);
+            }
+            else
+            {
+                var newTail = new ListNode(element);
+                newTail.Previous = tail;
+                tail.Next = newTail;
+                tail = newTail;
+            }
+
+            Count++;
+        }
+        public int RemoveFirst()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            var firstElement = head.Value;
+
+            head = head.Next;
+            if (head != null)
+            {
+                head.Previous = null;
+            }
+            else
+            {
+                tail = null;
+            }
+            Count--;
+
+            return firstElement;
+        }
+        public int RemoveLast()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            var lastElement = tail.Value;
+
+            tail = tail.Previous;
+            if (tail != null)
+            {
+                tail.Next = null;
+            }
+            else
+            {
+                head = null;
+            }
+            Count--;
+
+            return lastElement;
+        }
+        public void ForEach(Action<int> acton)
+        {
+            var element = head;
+            while (element != null)
+            {
+                acton(element.Value);
+                element = element.Next;
+            }
+        }
+        public int[] ToArray()
+        {        
+            int[] array = new int[Count];
+
+            int i = 0;
+            ForEach(x => array[i++] = x);
+
+
+            return array;
+        }
+    }
+}
---
+++ 3. C# Advanced/7.1 Implementing Linked List/CustomLinkedList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomDoublyLinkedList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+
+            doublyLinkedList.AddLast(10);
+            doublyLinkedList.AddLast(20);
+            doublyLinkedList.AddFirst(5);
+
+            doublyLinkedList.RemoveFirst();
+
+            doublyLinkedList.ForEach(x => Console.WriteLine(x));
+
+            int[] array = doublyLinkedList.ToArray();
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
