8394218bb6257ea4bef5cb38efc490f42952be08
Nikolay <nikolay.zlatev2005@gmail.com>
2023-06-07 22:48:46 +0300

    creating CustomList

---
+++ 3. C# Advanced/7.2 Implementing Stack and Queue/CustomList/CustomList.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomStackAndQueue
+{
+    public class CustomList
+    {
+        private const int InitialCapacity = 2;
+
+        private int[] items;
+        public CustomList()
+        {
+            items = new int[InitialCapacity];
+        }
+
+        public int Count { get; private set; }
+        public int Indexer { get; set; }
+
+        public int this[int index]
+        {
+            get
+            {
+                ThrowExceptionIfIndexOutOfRange(index);
+                return items[index];
+            }
+            set 
+            {
+                ThrowExceptionIfIndexOutOfRange(index);
+                items[index] = value;
+            }
+        }
+
+        public void Add(int element)
+        {
+            if (Count == items.Length)
+            {
+                Resize();
+            }
+
+            items[Count] = element;
+            Count++;
+        }
+        public int RemoveAt(int index)
+        {
+            ThrowExceptionIfIndexOutOfRange(index);
+
+            int element = items[index];
+            items[index] = default(int);
+            ShiftToLeft(index);
+
+            Count--;
+            if (Count <= items.Length / 4)
+            {
+                Shrink();
+            }
+
+            return element;
+        }
+        public void Insert(int index, int element)
+        {
+            ThrowExceptionIfIndexOutOfRange(index);
+
+            if (Count == items.Length)
+            {
+                Resize();
+            }
+            
+            ShiftToRight(index);
+            items[index] = element;
+            Count++;
+        }
+        public bool Contains(int element)
+        {
+            //return items.Contains(element);
+
+            for (int i = 0; i < Count; i++)
+            {
+                if (items[i] == element)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            ThrowExceptionIfIndexOutOfRange(firstIndex);
+            ThrowExceptionIfIndexOutOfRange(secondIndex);
+
+            int swapper = items[firstIndex];
+            items[firstIndex] = items[secondIndex];
+            items[secondIndex] = swapper;
+        }
+        private void Resize()
+        {
+            int[] copy = new int[items.Length * 2];
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                copy[i] = items[i];
+            }
+
+            items = copy;
+        }
+        private void Shrink()
+        {
+            int[] copy = new int[items.Length / 2];
+
+            for (int i = 0; i < Count; i++)
+            {
+                copy[i] = items[i];
+            }
+
+            items = copy;
+        }
+        private void ShiftToLeft(int index)
+        {
+            for (int i = index; i < items.Length; i++)
+            {
+                items[i] = items[i + 1];
+            }
+        }
+        private void ShiftToRight(int index)
+        {
+            for (int i = Count; i >= index; i--)
+            {
+                items[i] = items[i - 1];
+            }
+        }
+        private void ThrowExceptionIfIndexOutOfRange(int index)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new IndexOutOfRangeException("Invalid index");
+            }
+        }
+    }
+}
---
+++ 3. C# Advanced/7.2 Implementing Stack and Queue/CustomList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CustomStackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList list = new CustomList();
+
+            //1. public int Count { get { return items.Length; } private set { } } - to be access from user interface
+            //2. is it correct to have 2 elements by default - to ask later
+
+            list.Add(1);
+            list.Add(2);
+
+            list.Add(3);
+            list.Add(4);
+
+            list.Insert(2, 2);
+
+            
+        }
+    }
+}
