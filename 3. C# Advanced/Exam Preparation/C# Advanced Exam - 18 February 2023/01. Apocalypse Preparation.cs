b58f529093e0a8a2a5e93c617aec728f3b56e89e
Nikolay <nikolay.zlatev2005@gmail.com>
2023-06-13 11:52:24 +0300

    upload 1,2 from C# Advanced Exam - 18 February 2023
    exam preparation

---
+++ 3. C# Advanced/Exam Preparation/C# Advanced Exam - 18 February 2023/01. Apocalypse Preparation.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> itemCounts = new Dictionary<string, int>()
+            {
+                {"Patch", 0},
+                {"Bandage", 0},
+                {"MedKit", 0}
+            };
+
+            Queue<int> textiles = new Queue<int>(
+                Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList());
+
+            Stack<int> medicaments = new Stack<int>(
+                Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList());
+
+            string key = string.Empty;
+            while (textiles.Count > 0 && medicaments.Count > 0)
+            {
+                int currentTextile = textiles.Dequeue();
+                int currentMedicament = medicaments.Pop();
+
+                int result = currentMedicament + currentTextile;
+
+                if (result == 30)
+                {
+                    key = "Patch";
+                    itemCounts[key]++;
+                }
+                else if (result == 40)
+                {
+                    key = "Bandage";
+                    itemCounts[key]++;
+                }
+                else if (result == 100)
+                {
+                    key = "MedKit";
+                    itemCounts[key]++;
+                }
+                else
+                {
+                    if (result > 100)
+                    {
+                        int adding = result - 100;
+
+                        key = "MedKit";
+                        itemCounts[key]++;
+
+                        medicaments.Push(medicaments.Pop() + adding);
+                    }
+                    else
+                    {
+                        currentMedicament += 10;
+                        medicaments.Push(currentMedicament);
+                    }
+                }
+            }
+
+            bool isHavingTextileLeft = true;
+            bool isHavingMedLeft = true;
+
+            if (medicaments.Count == 0 && textiles.Count == 0)
+            {
+                isHavingTextileLeft = false;
+                isHavingMedLeft = false;
+                Console.WriteLine("Textiles and medicaments are both empty.");
+            }
+            else if (textiles.Count == 0)
+            {
+                isHavingTextileLeft = false;
+                Console.WriteLine("Textiles are empty.");
+            }
+            else
+            {
+                isHavingMedLeft = false;
+                Console.WriteLine("Medicaments are empty.");          
+            }
+
+            foreach (var item in itemCounts
+                .OrderByDescending(x => x.Value)
+                .ThenBy(x => x.Key))
+            {
+                if (item.Value != 0)
+                {
+                    Console.WriteLine($"{item.Key} - {item.Value}");
+                } 
+            }
+
+            if (isHavingTextileLeft)
+            {
+                Console.WriteLine("Textiles left: " + string.Join(", ", textiles));
+            }
+            if (isHavingMedLeft)
+            {
+                Console.WriteLine("Medicaments left: " + string.Join(", ", medicaments));
+            }
+        }
+    }
+}
---
+++ 3. C# Advanced/Exam Preparation/C# Advanced Exam - 18 February 2023/02. Blind Man's Buff.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = dimensions[0];
+            int m = dimensions[1];
+
+            char[,] playground = new char[n, m];
+
+            int blinderRow = 0;
+            int blinderCol = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                char[] current = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < m; col++)
+                {
+                    playground[row, col] = current[col];
+
+                    if (playground[row, col] == 'B')
+                    {
+                        blinderRow = row;
+                        blinderCol = col;
+                    }
+                }
+            }
+
+            List<string> commands = new List<string>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Finish")
+            {
+                commands.Add(command);
+            }
+
+            int touchOpponentCount = 0;
+            int moves = 0;
+
+            foreach (var currCommand in commands)
+            {
+                if (currCommand == "up")
+                {
+                    if (blinderRow - 1 < 0 || blinderRow - 1 >= n
+                        || blinderCol < 0 || blinderCol >= m
+                        || playground[blinderRow - 1, blinderCol] == 'O')
+                    {
+                        continue;
+                    }
+
+                    if (playground[blinderRow - 1, blinderCol] == 'P')
+                    {
+                        touchOpponentCount++;
+                    }
+
+                    moves++;
+
+                    playground[blinderRow, blinderCol] = '-';
+
+                    blinderRow--;
+                    playground[blinderRow, blinderCol] = 'B';
+                }
+                else if (currCommand == "down")
+                {
+
+                    if (blinderRow + 1 < 0 || blinderRow + 1 >= n
+                        || blinderCol < 0 || blinderCol >= m
+                        || playground[blinderRow + 1, blinderCol] == 'O')
+                    {
+                        continue;
+                    }
+
+                    if (playground[blinderRow + 1, blinderCol] == 'P')
+                    {
+                        touchOpponentCount++;
+                    }
+
+                    moves++;
+
+                    playground[blinderRow, blinderCol] = '-';
+
+                    blinderRow++;
+                    playground[blinderRow, blinderCol] = 'B';
+                }
+                else if (currCommand == "left")
+                {
+                    if (blinderRow < 0 || blinderRow >= n
+                        || blinderCol - 1 < 0 || blinderCol - 1 >= m
+                        || playground[blinderRow, blinderCol- 1] == 'O')
+                    {
+                        continue;
+                    }
+
+                    if (playground[blinderRow, blinderCol - 1] == 'P')
+                    {
+                        touchOpponentCount++;
+                    }
+
+                    moves++;
+
+                    playground[blinderRow, blinderCol] = '-';
+
+                    blinderCol--;
+                    playground[blinderRow, blinderCol] = 'B';
+                }
+                else if (currCommand == "right")
+                {
+                    if (blinderRow < 0 || blinderRow >= n
+                        || blinderCol + 1 < 0 || blinderCol + 1 >= m
+                        || playground[blinderRow, blinderCol + 1] == 'O')
+                    {
+                        continue;
+                    }
+
+                    if (playground[blinderRow, blinderCol + 1] == 'P')
+                    {
+                        touchOpponentCount++;
+                    }
+
+                    moves++;
+
+                    playground[blinderRow, blinderCol] = '-';
+
+                    blinderCol++;
+                    playground[blinderRow, blinderCol] = 'B';
+                }
+
+                if (touchOpponentCount == 3)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Game over!");
+            Console.WriteLine($"Touched opponents: {touchOpponentCount} Moves made: {moves}");
+        }
+    }
+}
