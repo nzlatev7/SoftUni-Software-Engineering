c4eb0b9696d9f2c3988721ea39e5a5a93cc11384
Nikolay <nikolay.zlatev2005@gmail.com>
2023-06-15 10:19:52 +0300

    upload 1, 2 from C# Advanced Retake Exam – 18 August 2022

---
+++ 3. C# Advanced/Exam Preparation/C# Advanced Retake Exam – 18 August 2022/01. Barista Contest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> drinkCounts = new Dictionary<string, int>()
+            {
+                { "Cortado", 0},
+                { "Espresso", 0},
+                { "Capuccino", 0},
+                { "Americano", 0},
+                { "Latte", 0},
+            };
+
+            Queue<int> coffee = new Queue<int>(
+                Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            Stack<int> milks = new Stack<int>(
+                Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            string key = string.Empty;
+            while (coffee.Count > 0 && milks.Count > 0)
+            {
+                int currentCoffee = coffee.Dequeue();
+                int currentMilk = milks.Pop();
+
+                int result = currentCoffee + currentMilk;
+
+                if (result == 50)
+                {
+                    key = "Cortado";
+                    drinkCounts[key]++;
+                }
+                else if (result == 75)
+                {
+                    key = "Espresso";
+                    drinkCounts[key]++;
+                }
+                else if (result == 100)
+                {
+                    key = "Capuccino";
+                    drinkCounts[key]++;
+                }
+                else if (result == 150)
+                {
+                    key = "Americano";
+                    drinkCounts[key]++;
+                }
+                else if (result == 200)
+                {
+                    key = "Latte";
+                    drinkCounts[key]++;
+                }
+                else
+                {
+                    milks.Push(currentMilk - 5);
+                }
+            }
+
+            if (milks.Count > 0 || coffee.Count > 0)
+            {
+                Console.WriteLine("Nina needs to exercise more! She didn't use all the coffee and milk!");
+            }
+            else
+            {
+                Console.WriteLine("Nina is going to win! She used all the coffee and milk!");
+            }
+
+            if (coffee.Count == 0)
+            {
+                Console.WriteLine("Coffee left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Coffee left: {string.Join(", ", coffee)}");
+            }
+
+            if (milks.Count == 0)
+            {
+                Console.WriteLine("Milk left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Milk left: {string.Join(", ", milks)}");
+            }
+
+            foreach (var drink in drinkCounts
+                .Where(x => x.Value > 0)
+                .OrderBy(x => x.Value)
+                .ThenByDescending(x => x.Key))
+            {
+                Console.WriteLine($"{drink.Key}: {drink.Value}");
+            }
+        }
+    }
+}
---
+++ 3. C# Advanced/Exam Preparation/C# Advanced Retake Exam – 18 August 2022/02. Help-A-Mole.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] field = new char[n, n];
+
+            int moleRow = 0;
+            int moleCol = 0;
+
+            List<SpecialLocation> specials = new List<SpecialLocation>();
+
+            for (int row = 0; row < n; row++)
+            {
+                string elements = Console.ReadLine();
+
+                for (int col = 0; col < n; col++)
+                {
+                    field[row, col] = elements[col];
+
+                    if (field[row, col] == 'M')
+                    {
+                        moleRow = row;
+                        moleCol = col;
+                    }
+
+                    if (field[row,col] == 'S')
+                    {
+                        specials.Add(new SpecialLocation(row, col));
+                    }
+                }
+            }
+
+            int points = 0;
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                if (points >= 25)
+                {
+                    break;
+                }
+
+                if (command == "left")
+                {
+                    if (!CheckOutOfField(n, moleRow, moleCol - 1))
+                    {
+                        Console.WriteLine("Don't try to escape the playing field!");
+                        continue;
+                    }
+
+                    field[moleRow, moleCol] = '-';
+                    moleCol--;
+                }
+                else if (command == "right")
+                {
+                    if (!CheckOutOfField(n, moleRow, moleCol + 1))
+                    {
+                        Console.WriteLine("Don't try to escape the playing field!");
+                        continue;
+                    }
+
+                    field[moleRow, moleCol] = '-';
+                    moleCol++;
+                }
+                else if (command == "up")
+                {
+                    if (!CheckOutOfField(n, moleRow - 1, moleCol))
+                    {
+                        Console.WriteLine("Don't try to escape the playing field!");
+                        continue;
+                    }
+
+                    field[moleRow, moleCol] = '-';
+                    moleRow--;
+                }
+                else if (command == "down")
+                {
+                    if (!CheckOutOfField(n, moleRow + 1, moleCol))
+                    {
+                        Console.WriteLine("Don't try to escape the playing field!");
+                        continue;
+                    }
+
+                    field[moleRow, moleCol] = '-';
+                    moleRow++;
+                }
+
+                if (char.IsDigit(field[moleRow, moleCol]))
+                {
+                    points += int.Parse(field[moleRow, moleCol].ToString());
+                }
+
+                if (field[moleRow, moleCol] == 'S')
+                {
+                    SpecialLocation first = specials[0];
+                    SpecialLocation second = specials[1];
+
+                    if (moleRow == first.Row && moleCol == first.Col)
+                    {
+                        field[moleRow, moleCol] = '-';
+
+                        moleRow = second.Row;
+                        moleCol = second.Col;
+
+                        points -= 3;
+                    }
+                    else
+                    {
+                        field[moleRow, moleCol] = '-';
+
+                        moleRow = first.Row;
+                        moleCol = first.Col;
+
+                        points -= 3;
+                    }
+                }
+
+                field[moleRow, moleCol] = 'M';
+            }
+
+            if (points >= 25)
+            {
+                Console.WriteLine("Yay! The Mole survived another game!");
+                Console.WriteLine($"The Mole managed to survive with a total of {points} points.");
+            }
+            else
+            {
+                Console.WriteLine("Too bad! The Mole lost this battle!");
+                Console.WriteLine($"The Mole lost the game with a total of {points} points.");
+            }
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write(field[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+        static bool CheckOutOfField(int n, int row, int col)
+        {
+            if (row < 0 || row >= n
+                || col < 0 || col >= n)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+    class SpecialLocation
+    {
+        public SpecialLocation(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+
+        public int Row { get; set; }
+        public int Col { get; set; }
+    }
+}
