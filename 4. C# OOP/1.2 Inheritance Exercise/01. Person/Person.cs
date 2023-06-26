df59df37eb0df1609e65f6c6a6272b7f4cc6a356
Nikolay <nikolay.zlatev2005@gmail.com>
2023-06-26 22:52:36 +0300

    upload two tasks from Inheritance - Exercise

---
+++ 4. C# OOP/1.2 Inheritance Exercise/01. Person/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        public Person(int age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 0)
+                {
+                    age = value;
+                }
+            }
+        }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append(String.Format($"Name: {Name}, Age: {Age}"));
+
+            return stringBuilder.ToString();
+        }
+    }
+}
