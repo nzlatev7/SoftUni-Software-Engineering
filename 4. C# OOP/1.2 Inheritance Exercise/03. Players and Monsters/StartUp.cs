e5ffd36bbc9887018f0567346d6ffed14ce0d0b4
Nikolay <nikolay.zlatev2005@gmail.com>
2023-06-27 17:45:23 +0300

    upload two tasks from Inheritance - Exercise

---
+++ 4. C# OOP/1.2 Inheritance Exercise/03. Players and Monsters/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Knight knight = new Knight("fef", 3);
+            Console.WriteLine(knight.Level);
+
+            Console.WriteLine(knight);
+        }
+    }
+}
---
+++ 4. C# OOP/1.2 Inheritance Exercise/04. Need for Speed/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Car car = new Car(100, 30);
+
+            car.Drive(2);
+
+            Console.WriteLine(car.Fuel);
+
+            SportCar sportCar = new SportCar(120, 120);
+            sportCar.Drive(10);
+            Console.WriteLine(sportCar.Fuel);
+
+        }
+    }
+}
