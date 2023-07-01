21d475941b04611e2faab7a448ff135a4afe3fae
Nikolay <nikolay.zlatev2005@gmail.com>
2023-07-01 16:20:20 +0300

    upload from Interfaces and Abstraction - Lab

---
+++ 4. C# OOP/3.1 Interfaces and Abstraction/02. Cars/ICar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public interface ICar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public string Start();
+        public string Stop();
+
+    }
+}
---
+++ 4. C# OOP/3.1 Interfaces and Abstraction/02. Cars/Seat.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            StringBuilder info = new StringBuilder();
+
+            info.AppendLine($"{Color} Seat {Model}");
+            info.AppendLine(Start());
+            info.Append(Stop());
+
+            return info.ToString();
+        }
+    }
+}
