f63fbfaf85ba8b024e8aaf70ab0cd2b80526315a
Nikolay <nikolay.zlatev2005@gmail.com>
2023-07-01 11:34:08 +0300

    upload from Encapsulation - Exercise

---
+++ 4. C# OOP/2.2 Encapsulation Exercise/01. Class Box Data/Box.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1 //???
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length 
+        {
+            get { return length; }
+            private set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+                length = value;
+            }
+        }
+        public double Width
+        {
+            get { return width; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+                width = value;
+            }
+        }
+        public double Height
+        {
+            get { return height; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            //2lw + 2lh + 2wh
+            return 2 * (Length * Width + Length * Height + Height * Width);
+        }
+        public double LateralSurfaceArea()
+        {
+            //2lh + 2wh
+            return 2 * (Length * Height + Height * Width);
+        }
+        public double Volume()
+        {
+            //lwh
+            return Length * Height * Width;
+        }
+    }
+}
---
+++ 4. C# OOP/2.2 Encapsulation Exercise/01. Class Box Data/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double heigh = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, heigh);
+
+                Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.Volume():f2}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }   
+        }
+    }
+}
