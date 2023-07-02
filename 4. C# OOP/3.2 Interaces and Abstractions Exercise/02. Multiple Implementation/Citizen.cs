b2f4bfc1f8f8ae04d59ce31a614365d432ad9d33
Nikolay <nikolay.zlatev2005@gmail.com>
2023-07-02 23:16:05 +0300

    upload 2 tasks from Interfaces and Abstraction - Exercise

---
+++ 4. C# OOP/3.2 Interaces and Abstractions Exercise/02. Multiple Implementation/Citizen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson, IBirthable, IIdentifiable
+    {
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Birthdate = birthdate;        
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Birthdate { get; set; }
+    }
+}
