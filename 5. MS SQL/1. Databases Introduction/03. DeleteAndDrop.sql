f6fc9bc8a448a7e61cd2394f9ee05e2200590f49
Nikolay Zlatev <106867192+nzlatev7@users.noreply.github.com>
2023-09-06 22:35:56 +0300

    Delete 5. MS SQL/1. Databases Introduction directory

--- 5. MS SQL/1. Databases Introduction/03. DeleteAndDrop.sql
+++
@@ -1,5 +0,0 @@
---truncate the table -> delete all data in this table
-DELETE FROM Minions;
-
-DROP TABLE Minions;
-DROP TABLE Towns;
--- 5. MS SQL/1. Databases Introduction/08. DefaultConstraint.sql
+++
@@ -1,7 +0,0 @@
---adding default constraint to be the current datetime
-ALTER TABLE Users 
-ADD CONSTRAINT DF_LastLoginTimeNow DEFAULT GETDATE() FOR LastLoginTime;
-
-INSERT INTO Users(Username,[Password],ProfilePicture,IsDeleted)
-VALUES
-('PESHO1234','123456', NULL, 0);
