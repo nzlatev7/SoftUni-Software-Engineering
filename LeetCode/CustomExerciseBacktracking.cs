22ba3b9031db830f35834c5ee879381148290a24
unknown <nikolay.zlatev2005@gmail.com>
2024-04-26 16:36:19 +0300

    WordSearchAndBacktracking

---
+++ LeetCode/CustomExerciseBacktracking.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    internal class Backtracking
+    {
+        static int N;
+        public static void Mine()
+        {
+            N = int.Parse(Console.ReadLine());
+            int[,] board = new int[N, N];
+
+            if (!TheBoardSolver(board, 0))
+            {
+                Console.WriteLine("Solution not found.");
+            }
+            PrintBoard(board);
+            Console.ReadLine();
+        }
+
+        public static bool TheBoardSolver(int[,] board, int col)  
+        {
+            if (col >= N)
+            {
+                return true;
+            }
+
+            // for every row (move the row)
+            for (int i = 0; i < N; i++)
+            {
+                if (IsValid(board, i, col))
+                {
+                    board[i, col] = 1;
+                    if (TheBoardSolver(board, col + 1))
+                    {
+                        return true;
+                    }
+
+                    board[i, col] = 0;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool IsValid(int[,] board, int row, int col)
+        {
+            // horizontally
+            for (int i = col; i >= 0; i--)
+            {
+                if (board[row, i] == 1)
+                {
+                    return false;
+                }
+            }
+
+            // diagonal 1
+            for (int i = col, j = row; i >= 0 && j >= 0; i--, j--)
+            {
+                if (board[j, i] == 1)
+                {
+                    return false;
+                }
+            }
+            // diagonal 2
+            for (int i = col, j = row; i < N && j >= 0; i++, j--)
+            {
+                if (board[j, i] == 1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static void PrintBoard(int[,] board)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(board[i, j] + " ");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ LeetCode/WordSearch.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TestApp
+{
+    public class WordSearch
+    {
+        public static bool Exist(char[][] board, string word)
+        {
+            int m = board.Length; // 3
+            int n = board[0].Length; // 4
+
+            Func<int, int, int, bool> backtrack = null;
+            // indeces of the potion, of the word
+            backtrack = (i, j, k) => {
+                // the bottom of our recursion
+                if (k == word.Length)
+                {
+                    return true;
+                }
+                // is in the valid field
+                if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[k])
+                {
+                    return false;
+                }
+
+                char temp = board[i][j];
+
+                // reset the value -> to not be possible to not go there again
+                board[i][j] = '\0';
+                bool result = backtrack(i + 1, j, k + 1) ||
+                              backtrack(i - 1, j, k + 1) ||
+                              backtrack(i, j + 1, k + 1) ||
+                              backtrack(i, j - 1, k + 1);
+
+                board[i][j] = temp;
+                return result;
+            };
+
+            for (int i = 0; i < m; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    if (backtrack(i, j, 0))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
